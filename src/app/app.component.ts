ba8cad0d45eff0a6eccdb7356eea332d2a6bd3fd
Sunil Mekala <smekala@innominds.com>
2020-07-05 19:15:30 +0530

    modified files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-  arrayOf9 = Array.of(1,2,3,4,5,6,7,8,9);
+export class AppComponent {
+  title = 'MyWeatherApp';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WeatherPanelComponent } from './weatherpanel/weatherpanel.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, WeatherPanelComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
