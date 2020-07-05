520cc828c840e27e7c6955ba39a8ec04495ad819
Guru Avinash Golla <iguruavinash@gmail.com>
2020-07-05 20:43:26 +0530

    Added city edit and api integration

--- src/app/components/weatherpanel/weatherpanel.component.ts
+++ src/app/components/weatherpanel/weatherpanel.component.ts
@@ -1,45 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 
-import {WeatherPanelService} from './weatherpanel.service';
+import { WeatherPanelService } from './weatherpanel.service';
 
 @Component({
   selector: 'app-weather-panel',
   templateUrl: './weatherpanel.component.html',
   styleUrls: ['./weatherpanel.component.scss'],
-  providers: [WeatherPanelService]
+  providers: [WeatherPanelService],
 })
-
 export class WeatherPanelComponent implements OnInit {
   cityName = 'London';
   weatherData: any = {
     main: {
-    "temp": 290.57,
-    "feels_like": 285.87,
-    "temp_min": 289.82,
-    "temp_max": 291.15,
-    "pressure": 1008,
-    "humidity": 77
-  },
-    weather: {id: 800, description: 'clear sky'}
+      temp: 37,
+    },
+    weather: { id: 799, description: 'clear sky' },
+    isCityEditable: false,
   };
 
-  constructor(
-    private weatherPanelService: WeatherPanelService) {
-
-    }
-  ngOnInit() {
-
-  }
+  constructor(private weatherPanelService: WeatherPanelService) {}
+  ngOnInit() {}
 
   onCityNameChange() {
-    if(this.cityName) {
-      this.weatherPanelService.getWeatherByCityName(this.cityName).then((response: any) => {
-        this.weatherData = {
-          main: response.main,
-          weather: response.weather[0] ? response.weather[0]: {id: 800, description: 'clear sky'}
-        };
-      });
+    if (this.cityName) {
+      this.weatherPanelService
+        .getWeatherByCityName(this.cityName)
+        .then((response: any) => {
+          this.weatherData = {
+            main: response.main,
+            weather: response.weather[0]
+              ? response.weather[0]
+              : { id: 800, description: 'clear sky' },
+          };
+        });
     }
   }
 
+  editCityName() {
+    this.weatherData.isCityEditable = true;
+  }
+
+  onKeyUp(event) {
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      console.log(event, this.cityName);
+      // alert('Hi');
+      this.onCityNameChange();
+    }
+  }
 }
--- src/app/components/weatherpanel/weatherpanel.service.ts
+++ src/app/components/weatherpanel/weatherpanel.service.ts
@@ -2,6 +2,7 @@ import { Injectable, EventEmitter } from '@angular/core';
 import 'rxjs/add/operator/toPromise';
 import { HttpClient } from '@angular/common/http';
 import {APP_CONFIG} from '../../config';
+import {environment} from '../../../environments/environment';
 
 @Injectable()
 export class WeatherPanelService {
@@ -10,7 +11,7 @@ export class WeatherPanelService {
   }
 
   getWeatherByCityName(cityName: string) {
-    const url = 'api.openweathermap.org/data/2.5/weather?units=metric&q=' + cityName + '&appid=' + APP_CONFIG.API_KEY;
+    const url = environment.host + cityName + '&appid=' + APP_CONFIG.API_KEY;
     return this.httpClient.get(url).toPromise();
   }
 
